0476bd492d2b4effbd6add1aa7ae6ff768f3e263
Yang Sheng Han <progamesigner@gmail.com>
2015-03-08 16:07:26 +0800

    Added functions.zsh file.

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,5 +1,13 @@
 #! /bin/zsh
 
+function retval () {
+    if [[ -z ${1} ]];then
+        print "."
+    else
+        print "${1}"
+    fi
+}
+
 function ps-by-user () {
     ps -U "${1:-$USER}" -o "pid,%cpu,%mem,command" "${(@)argv[2,-1]}"
 }
@@ -14,6 +22,18 @@ function zsh-stats () {
 
 function find-and-execute () {
     find . -type f -iname "*${1:-}*" -exec "${2:-file}" "{}" \;
+}
+
+function find-rb-files () {
+    find . -name "*.rb"
+}
+
+function find-py-files () {
+    find . -name "*.py"
+}
+
+function find-php-files () {
+    find . -name "*.php"
 }
 
 function align-right-padding () {
@@ -43,3 +63,23 @@ function align-center-indent () {
     for _ in {0..${padding}}; do spacing="${spacing} "; done
     print "${spacing}"
 }
+
+function clean-python-cache () {
+    ZSH_PYCLEAN_PLACES=${*:-"."}
+    find ${ZSH_PYCLEAN_PLACES} -type f -name "*.py[co]" -delete
+    find ${ZSH_PYCLEAN_PLACES} -type d -name "__pycache__" -delete
+}
+
+function remove-zero-files () {
+    find "$(retval $1)" -type f -size 0 -exec rm -rf {} \;
+}
+
+function kill-zombie-processes() {
+    ps -eal | awk "{ if (\$2 == "Z") {print \$4}}" | kill -9
+}
+
+if [ -z "\${which tree}" ]; then
+    tree () {
+        find $@ -print | sed -e "s;[^/]*/;|____;g;s;____|; |;g"
+    }
+fi
