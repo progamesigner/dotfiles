d419f68ae74d9470fc5b23f7a89266a64ddac6fd
Yang Sheng Han <progamesigner@gmail.com>
2015-03-09 15:43:42 +0800

    Adjusted LS colors.

--- zsh/appearances.zsh
+++ zsh/appearances.zsh
@@ -48,8 +48,9 @@ function terminal_title_cwd () {
 # ls colors
 # =========
 autoload -U colors && colors
-export LSCOLORS="exfxcxdxbxGxDxabagacad"
-export LS_COLORS="di=34:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:"
+export LSCOLORS="exfxcxdxbxegedabagacad"
+export LS_COLORS="di=34;40:ln=35;40:so=32;40:pi=33;40:ex=31;40:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:"
+export GREP_COLOR="1;33"
 
 # Find the option for using colors in ls, depending on the version: Linux or BSD
 if [[ "$(uname -s)" == "NetBSD" ]]; then
