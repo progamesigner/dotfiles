351ea76da9e3ae99c240e70f5906704d1a89b842
Yang Sheng Han <progamesigner@gmail.com>
2015-03-10 09:46:54 +0800

    Updated SSH Agent plugin.

--- zsh/plugins/ssh-agent/ssh-agent.plugin.zsh
+++ zsh/plugins/ssh-agent/ssh-agent.plugin.zsh
@@ -2,42 +2,32 @@
 
 # Initializations
 # ===============
-local _plugin__ssh_env
-local _plugin__forwarding
+if [[ "$OSTYPE" == darwin* ]] || (( ! $+commands[ssh-agent] )); then return 1; fi
+_ssh_dir="${HOME}/.ssh"
+_ssh_agent_env="${_ssh_agent_env:-${TMPDIR:-/tmp}/ssh-agent.env}"
+_ssh_agent_sock="${TMPDIR:-/tmp}/ssh-agent.sock"
 
-zstyle -b :omz:plugins:ssh-agent agent-forwarding _plugin__forwarding
-if (( $+commands[scutil] )); then
-    _plugin__ssh_env="${HOME}/.ssh/environment-$(scutil --get ComputerName)"
-else
-    _plugin__ssh_env="${HOME}/.ssh/environment-${HOST}"
+if [[ ! -S "${SSH_AUTH_SOCK}" ]]; then
+    source "${_ssh_agent_env}" 2> /dev/null
+
+    if ! ps -U "${LOGNAME}" -o pid,ucomm | grep -q -- "${{SSH_AGENT_PID}:--1} ssh-agent"; then
+        eval "$(ssh-agent | sed "/^echo /d" | tee "${_ssh_agent_env}")"
+    fi
 fi
 
-if [[ ${_plugin__forwarding} == "yes" && -n "$SSH_AUTH_SOCK" ]]; then
-    [[ -L $SSH_AUTH_SOCK ]] || ln -sf "$SSH_AUTH_SOCK" "/tmp/ssh-agent-$USER-screen"
-elif [ -f "${_plugin__ssh_env}" ]; then
-    source ${_plugin__ssh_env} >/dev/null
-    $(ps x | grep ${SSH_AGENT_PID} | grep ssh-agent >/dev/null || { _plugin__start_agent; })
-else
-    _plugin__start_agent;
+if [[ -S "${SSH_AUTH_SOCK}" && "${SSH_AUTH_SOCK}" != "${_ssh_agent_sock}" ]]; then
+    ln -sf "${SSH_AUTH_SOCK}" "${_ssh_agent_sock}"
+    export SSH_AUTH_SOCK="${_ssh_agent_sock}"
 fi
 
-# Functions
-# =========
-function _plugin__start_agent () {
-    local -a identities
-    local lifetime
-    zstyle -s :omz:plugins:ssh-agent lifetime lifetime
+if ssh-add -l 2>&1 | grep -q "The agent has no identities"; then
+    if (( ${#ZSH_SSH_AGENT_IDENTITIES} > 0 )); then
+        ssh-add "$_ssh_dir/${^ZSH_SSH_AGENT_IDENTITIES[@]}" 2> /dev/null
+    else
+        ssh-add 2> /dev/null
+    fi
+fi
 
-    $(/usr/bin/env ssh-agent ${lifetime:+-t} ${lifetime} | sed "s/^echo/#echo/" > ${_plugin__ssh_env})
-    chmod 600 "${_plugin__ssh_env}"
-    source "${_plugin__ssh_env}" >/dev/null
-
-    zstyle -a :omz:plugins:ssh-agent identities identities
-    print -R -e starting ssh-agent...
-
-    $(/usr/bin/ssh-add "${HOME}/.ssh/${^identities}")
-}
-
-unfunction _plugin__start_agent
-unset _plugin__forwarding
-unset _plugin__ssh_env
+unset _ssh_dir
+unset _ssh_agent_env
+unset _ssh_agent_sock
