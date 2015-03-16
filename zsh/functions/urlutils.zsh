2e22bfdae6c210cd37498f1a21b03b2404a79ec6
Yang Sheng Han <progamesigner@gmail.com>
2015-03-16 11:27:15 +0800

    Normalised code style.

--- zsh/functions/urlutils.zsh
+++ zsh/functions/urlutils.zsh
@@ -1,29 +1,29 @@
 #! /bin/zsh
 
-if [[ $(whence ${URLTOOLS_METHOD}) = "" ]]; then
+if [[ $+commands[${URLTOOLS_METHOD}] ]]; then
     URLTOOLS_METHOD="shell"
 fi
 
-if [[ $(whence node) != "" && ( "x${URLTOOLS_METHOD}" = "x"  || "x${URLTOOLS_METHOD}" = "xnode" ) ]]; then
+if [[ $+commands[node] && ( "x${URLTOOLS_METHOD}" = "x"  || "x${URLTOOLS_METHOD}" = "xnode" ) ]]; then
     alias urlencode="node -e \"console.log(encodeURIComponent(process.argv[1]))\""
     alias urldecode="node -e \"console.log(decodeURIComponent(process.argv[1]))\""
-elif [[ $(whence python) != "" && ( "x${URLTOOLS_METHOD}" = "x" || "x${URLTOOLS_METHOD}" = "xpython" ) ]]; then
+elif [[ $+commands[python] && ( "x${URLTOOLS_METHOD}" = "x" || "x${URLTOOLS_METHOD}" = "xpython" ) ]]; then
     alias urlencode="python -c \"import sys, urllib as ul; print ul.quote_plus(sys.argv[1])\""
     alias urldecode="python -c \"import sys, urllib as ul; print ul.unquote_plus(sys.argv[1])\""
-elif [[ $(whence xxd) != "" && ( "x${URLTOOLS_METHOD}" = "x" || "x${URLTOOLS_METHOD}" = "xshell" ) ]]; then
+elif [[ $+commands[xxd] && ( "x${URLTOOLS_METHOD}" = "x" || "x${URLTOOLS_METHOD}" = "xshell" ) ]]; then
     function urlencode () {
         echo $@ | tr -d "\n" | xxd -plain | sed "s/\(..\)/%\1/g"
     }
     function urldecode () {
         printf $(echo -n $@ | sed "s/\\\\/\\\\\\\\/g;s/\\(%\)\\([0-9a-fA-F][0-9a-fA-F]\)/\\\\x\\2/g")"\n"
     }
-elif [[ $(whence ruby) != "" && ( "x${URLTOOLS_METHOD}" = "x" || "x${URLTOOLS_METHOD}" = "xruby" ) ]]; then
+elif [[ $+commands[ruby] && ( "x${URLTOOLS_METHOD}" = "x" || "x${URLTOOLS_METHOD}" = "xruby" ) ]]; then
     alias urlencode="ruby -r cgi -e \"puts CGI.escape(ARGV[0])\""
     alias urldecode="ruby -r cgi -e \"puts CGI.unescape(ARGV[0])\""
-elif [[ $(whence php) != "" && ( "x${URLTOOLS_METHOD}" = "x" || "x${URLTOOLS_METHOD}" = "xphp" ) ]]; then
+elif [[ $+commands[php] && ( "x${URLTOOLS_METHOD}" = "x" || "x${URLTOOLS_METHOD}" = "xphp" ) ]]; then
     alias urlencode="php -r \"echo rawurlencode(\\\$argv[1]); echo \\\"\\n\\\";\""
     alias urldecode="php -r \"echo rawurldecode(\\\$argv[1]); echo \\\"\\n\\\";\""
-elif [[ $(whence perl) != "" && ( "x${URLTOOLS_METHOD}" = "x" || "x${URLTOOLS_METHOD}" = "xperl" ) ]]; then
+elif [[ $+commands[perl] && ( "x${URLTOOLS_METHOD}" = "x" || "x${URLTOOLS_METHOD}" = "xperl" ) ]]; then
     if perl -MURI::Encode -e 1&>/dev/null; then
         alias urlencode="perl -MURI::Encode -ep \"uri_encode($ARGV[0]);\""
         alias urldecode="perl -MURI::Encode -ep \"uri_decode($ARGV[0]);\""
