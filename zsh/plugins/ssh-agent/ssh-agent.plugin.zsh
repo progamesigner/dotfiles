87e1652e41c486029b7b1c264887e77d1acc814c
Yang Sheng Han <progamesigner@gmail.com>
2015-08-04 09:38:57 +0800

    Fixed warning and error message in gpg-agent and ssh-agent plugins.

--- zsh/plugins/ssh-agent/ssh-agent.plugin.zsh
+++ zsh/plugins/ssh-agent/ssh-agent.plugin.zsh
@@ -10,7 +10,7 @@ if [[ "${OSTYPE}" == darwin* ]] || (( ! $+commands[ssh-agent] )); then
     if [[ ! -S "${SSH_AUTH_SOCK}" ]]; then
         source "${_ssh_agent_env}" 2> /dev/null
 
-        if ! ps -U "${LOGNAME}" -o pid,ucomm | grep -q -- "${{SSH_AGENT_PID}:--1} ssh-agent"; then
+        if ! ps -U "${USER}" -o pid,ucomm | grep -q -- "${SSH_AGENT_PID:--1} ssh-agent"; then
             eval "$(ssh-agent | sed "/^echo /d" | tee "${_ssh_agent_env}")"
         fi
     fi
