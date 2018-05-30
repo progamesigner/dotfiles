e4c608dcc10643b72fa374952b79095d9cb0cd5a
Yang Sheng Han <progamesigner@gmail.com>
2018-05-30 18:13:02 +0800

    Update git configurations

--- git/install.sh
+++ git/install.sh
@@ -1,20 +1,9 @@
 #! /bin/sh
 
-GIT_CREDENTIAL_HELPER="cache"
-if [ "$(uname -s)" == "Darwin" ]; then
-    GIT_CREDENTIAL_HELPER="osxkeychain"
-fi
 user "What is your name for git"
 read -e GIT_USER_NAME
 user "What is your email for git"
 read -e GIT_USER_EMAIL
-
-if [ -x $(command -v brew) ]; then
-    brew install git
-    brew install git-flow
-    brew install git-extras
-    brew install hub
-fi
 
 if [[ -n ${GIT_USER_NAME} ]]; then
     git config --global user.name "${GIT_USER_NAME}"
@@ -27,8 +16,6 @@ git config --global apply.whitespace "nowarn"
 
 git config --global branch.autosetupmerge "true"
 
-git config --global credential.helper "${GIT_CREDENTIAL_HELPER}"
-
 git config --global color.ui "auto"
 git config --global color.diff "auto"
 git config --global color.branch "auto"
@@ -36,7 +23,7 @@ git config --global color.status "auto"
 
 git config --global core.excludesfile "~/.gitignore"
 git config --global core.attributesfile "~/.gitattributes"
-git config --global core.editor "${EDITOR:-"vim"}"
+git config --global core.editor ${EDITOR:-"vim"}
 git config --global core.autocrlf "input"
 git config --global core.safecrlf "warn"
 git config --global core.whitespace "space-before-tab,-indent-with-non-tab,trailing-space"
@@ -55,6 +42,8 @@ git config --global color.status.added "yellow"
 git config --global color.status.changed "green"
 git config --global color.status.untracked "blue"
 
+git config --global commit.gpgsign true
+
 git config --global diff.renames "copies"
 
 git config --global merge.enabled "true"
@@ -64,6 +53,10 @@ git config --global rerere.log "true"
 git config --global help.autocorrect "50"
 
 git config --global push.default "simple"
+
+git config --global url."git@github.com:".insteadOf "https://github.com/"
+
+git config --global user.useconfigonly true
 
 info "Linking ${DOTTARGET}/.gitignore ..."
 if [ -f "${DOTTARGET}/.gitignore" ] || [ -h "${DOTTARGET}/.gitignore" ]; then
