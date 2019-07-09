36f6847cafe1b574da986ae7bdcc379127de00f3
Yang Sheng Han <progamesigner@gmail.com>
2019-07-09 17:29:55 +0800

    Fix GnuPG not using agent on macOS

--- gnupg/install.sh
+++ gnupg/install.sh
@@ -7,6 +7,21 @@ if [ -z "$NO_GNUPG" ]; then
 
     if [[ "$(uname -s)" == *Darwin* ]]; then
         brew install gpg gpgme pinentry-mac
+
+        info "Make sure \"$DOTTARGET/.gnupg\" exists"
+        mkdir -p "$DOTTARGET/.gnupg"
+
+        touch "$DOTTARGET/.gnupg/gpg.conf"
+        ret=$(grep -q "^use-agent" "$DOTTARGET/.gnupg/gpg.conf")
+        if [ $? -ne 0 ]; then
+            echo "use-agent" >> $DOTTARGET/.gnupg/gpg.conf
+        fi
+
+        touch "$DOTTARGET/.gnupg/gpg-agent.conf"
+        ret=$(grep -q "^pinentry-program" "$DOTTARGET/.gnupg/gpg-agent.conf")
+        if [ $? -ne 0 ]; then
+            echo "pinentry-program /usr/local/bin/pinentry-mac" >> $DOTTARGET/.gnupg/gpg-agent.conf
+        fi
     else
         info "No supported platform found, skipped ..."
     fi
