1196950b7ba214b474d5eaaeea723d423626e2cb
Yang Sheng Han <progamesigner@gmail.com>
2019-07-09 13:50:22 +0800

    Add Kubernetes installation

---
+++ kubernetes/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+head "Setting up Kubernetes"
+
+if [ -z "$NO_KUBERNETES" ]; then
+    info "Install Kubernetes (kubectl, draft, and helm)"
+
+    if [[ "$(uname -s)" == *Darwin* ]]; then
+        brew install azure/draft/draft kubernetes-cli kubernetes-helm
+    else
+        info "No supported platform found, skipped ..."
+    fi
+
+    info "Installed Kubernetes"
+
+    info "Make sure \"$PWD/kubernetes/config\" exists"
+    touch $PWD/kubernetes/config
+
+    link $PWD/kubernetes/config "$DOTTARGET/.kube/config"
+fi
