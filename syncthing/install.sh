8b7f1548b807d7f741d6225218ae5017941f6e5b
Yang Sheng Han <progamesigner@gmail.com>
2019-07-09 11:43:18 +0800

    Add syncthing installation

--- syncthing/install.sh
+++ syncthing/install.sh
@@ -1,7 +1,37 @@
-#! /bin/sh
+#!/bin/sh
 
-if [ -n ${DOTFILE_FORCE_OVERRIDE} ]; then
-    ln -s ${DOTFILES}/syncthing/stignore ~/.stignore
-else
-    ln -f -s ${DOTFILES}/syncthing/stignore ~/.stignore
+head "Setting up Syncthing"
+
+if [ -z "$NO_SYNCTHING" ]; then
+    info "Install Syncthing"
+
+    if [[ "$(uname -s)" == *Darwin* ]]; then
+        brew install syncthing
+    else
+        info "No supported platform found, skipped ..."
+    fi
+
+    info "Installed Syncthing"
+
+    info "Start Syncthing"
+    if [[ "$(uname -s)" == *Darwin* ]]; then
+        brew services start syncthing
+    else
+        info "No supported platform found, skipped ..."
+    fi
+    info "Started Syncthing"
+
+    if [[ "$(uname -s)" == *Darwin* ]] || [[ "$(uname -s)" == *Linux* ]]; then
+        info "Link \"$DOTFILES/syncthing/stignore\" to \"$(echo $HOME/.stignore)\""
+
+        if [ -f $HOME/.stignore ]; then
+            info "Backed up \"$(echo $HOME/.stignore)\" because file already existed"
+
+            mv $HOME/.stignore $HOME/.stignore.bak
+        fi
+
+        ln -s $DOTFILES/syncthing/stignore $HOME/.stignore
+
+        info "Linked \"$(echo $HOME/.stignore)\""
+    fi
 fi
