9a17003ab6af36a7614b2051e277cc14ee147306
Yang Sheng Han <progamesigner@gmail.com>
2019-07-09 13:45:53 +0800

    Add SSH installation

---
+++ ssh/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+head "Setting up SSH"
+
+if [ -z "$NO_SSH" ]; then
+    info "Make sure \"$PWD/ssh/config\" exists"
+    touch $PWD/ssh/config
+
+    link $PWD/ssh/config "$DOTTARGET/.ssh/config"
+fi
