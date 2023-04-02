38b9df8d61ff2c33e12cf834b4e8a12b88856097
Yang Sheng Han <progamesigner@gmail.com>
2023-04-02 08:46:28 +0000

    Rework Docker

--- docker/install.sh
+++ docker/install.sh
@@ -1,15 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 head "Setting up Docker"
 
 if [ -z "$NO_DOCKER" ]; then
-    info "Install Docker (docker-cli)"
-
-    if [[ "$(uname -s)" == *Darwin* ]]; then
-        brew install docker
-    else
-        info "No supported platform found, skipped ..."
-    fi
-
-    info "Installed Docker"
+    touch "$PWD/docker/config/config.json"
 fi
