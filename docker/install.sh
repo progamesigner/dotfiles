d5ab6542739bec33fa46cca9b14290c671da53bc
Yang Sheng Han <progamesigner@gmail.com>
2023-04-02 14:42:39 +0000

    Remove Docker, Kubernetes, and Syncthing

--- docker/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-head "Setting up Docker"
-
-if [ -z "$NO_DOCKER" ]; then
-    touch "$PWD/docker/config/config.json"
-fi
--- kubernetes/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-head "Setting up Kubernetes"
-
-if [ -z "$NO_KUBERNETES" ]; then
-    touch "$PWD/kubernetes/config"
-fi
