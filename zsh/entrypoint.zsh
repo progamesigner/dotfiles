b756c6711d1fb731997ec74daf7d25d3b22222ec
Yang Sheng Han <progamesigner@gmail.com>
2023-04-03 01:53:34 +0800

    Load ZSH_CUSTOM script

--- zsh/entrypoint.zsh
+++ zsh/entrypoint.zsh
@@ -7,4 +7,8 @@ setopt PROMPT_SUBST         # Setup the prompt with pretty colors
 source $zsh/aliases.zsh
 source $zsh/functions.zsh
 
+if [ -n $ZSH_CUSTOM ] && [ -f $ZSH_CUSTOM/entrypoint.zsh ]; then
+    source $ZSH_CUSTOM/entrypoint.zsh
+fi
+
 PROMPT="%(?.%F{magenta}.%F{red})❯%f "
