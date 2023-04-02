440363faae510e5a17a8f9d2b918ef3de459e7ba
Yang Sheng Han <progamesigner@gmail.com>
2023-04-02 08:47:11 +0000

    Rework Kubernetes

--- kubernetes/install.sh
+++ kubernetes/install.sh
@@ -1,20 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 head "Setting up Kubernetes"
 
 if [ -z "$NO_KUBERNETES" ]; then
-    info "Install Kubernetes (kubectl)"
-
-    if [[ "$(uname -s)" == *Darwin* ]]; then
-        brew install kubernetes-cli
-    else
-        info "No supported platform found, skipped ..."
-    fi
-
-    succ "Installed Kubernetes"
-
-    info "Make sure \"$PWD/kubernetes/config\" exists"
-    touch $PWD/kubernetes/config
-
-    link $PWD/kubernetes/config "$DOTTARGET/.kube/config"
+    touch "$PWD/kubernetes/config"
 fi
