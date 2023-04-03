4d2017b19444de85711ec41546cd83accd61b169
Yang Sheng Han <progamesigner@gmail.com>
2023-04-03 18:04:25 +0800

    Update ZSH

--- zsh/completions.zsh
+++ zsh/completions.zsh
@@ -1,8 +1,21 @@
 #!/usr/bin/env zsh
 
+autoload -U +X compinit && compinit
+
+zmodload -i zsh/complist
+
+zstyle ':completion:*:*:*:*:*' menu select
+
 zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]-_}={[:upper:][:lower:]_-}' 'r:|=*' 'l:|=* r:|=*'
 zstyle ':completion:*' special-dirs true
 zstyle ':completion:*' list-colors ''
 zstyle ':completion:*:cd:*' tag-order local-directories directory-stack path-directories
+
+zstyle ':completion:*' use-cache yes
+if [ -n "$ZSH_CACHE_DIR" ]; then
+    zstyle ':completion:*' cache-path $ZSH_CACHE_DIR
+fi
+
+zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
 
 autoload -U +X bashcompinit && bashcompinit
--- zsh/entrypoint.zsh
+++ zsh/entrypoint.zsh
@@ -2,8 +2,13 @@
 
 local zsh=$(dirname $0)
 
+autoload -U +X colors && colors
+
+setopt ALWAYS_TO_END        # Always place the cursor to the end of the word completed
 setopt AUTO_CD              # Auto changes to a directory without typing cd
+setopt AUTO_MENU            # Show completion menu on successive tab press
 setopt AUTO_PUSHD           # Push the old directory onto the stack on cd
+setopt COMPLETE_IN_WORD     # Fill in the word at the point of the cursor
 setopt PROMPT_SUBST         # Setup the prompt with pretty colors
 setopt PUSHD_IGNORE_DUPS    # Do not store duplicates in the stack
 setopt PUSHD_SILENT         # Do not print the directory stack after pushd or popd
@@ -23,6 +28,8 @@ if [ -n "$ZSH_CUSTOM" ]; then
     for name in $plugins; do
         if [ -f "$ZSH_CUSTOM/plugins/$name/$name.plugin.zsh" ]; then
             source "$ZSH_CUSTOM/plugins/$name/$name.plugin.zsh"
+        elif [ -f "$ZSH_CUSTOM/plugins/$name/_$name" ]; then
+            # only completion, skipped ...
         else
             echo "[ZSH] Plugin \"$name\" not found"
         fi
