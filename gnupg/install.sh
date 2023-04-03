88fcd30b99533afaacd180cb5be645f9a43c5dcf
Yang Sheng Han <progamesigner@gmail.com>
2023-04-03 13:57:11 +0000

    Update GnuPG scripts

--- gnupg/install.sh
+++ gnupg/install.sh
@@ -5,15 +5,13 @@ head "Setting up GnuPG"
 if [ -z "$NO_GNUPG" ]; then
     info "Configuring GnuPG"
 
-    if [[ "$(uname -s)" == *Darwin* ]]; then
-        ensure "$DOTTARGET/.gnupg/gpg-agent.conf"
-        touch "$DOTTARGET/.gnupg/gpg-agent.conf"
-        ret=$(grep -q "^pinentry-program" "$DOTTARGET/.gnupg/gpg-agent.conf")
-        if [ $? -ne 0 ]; then
-            cat <<-EOF >> "$DOTTARGET/.gnupg/gpg-agent.conf"
+    ensure "$DOTTARGET/.gnupg/gpg-agent.conf"
+    touch "$DOTTARGET/.gnupg/gpg-agent.conf"
+    ret=$(grep -q "^pinentry-program" "$DOTTARGET/.gnupg/gpg-agent.conf")
+    if [ $? -ne 0 ] && [ -n "$(which pinentry-mac)" ]; then
+        cat <<-EOF >> "$DOTTARGET/.gnupg/gpg-agent.conf"
 pinentry-program $(which pinentry-mac)
 EOF
-        fi
     fi
 
     info "Configured GnuPG"
