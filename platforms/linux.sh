5692ed4073145835cfda6fb311c4eaa0f6675deb
Yang Sheng Han <progamesigner@gmail.com>
2023-04-03 13:55:14 +0000

    Remove Homebrew from platform scripts

--- platforms/linux.sh
+++ platforms/linux.sh
@@ -1,18 +1,3 @@
 #!/usr/bin/env sh
 
 head "Setting up Linux"
-
-if [ -z "$NO_HOMEBREW" ] && [ ! -x "$(command -v brew)" ]; then
-    info "Install Homebrew"
-
-    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    if [ $? -ne 0 ]; then
-        fail "Homebrew installation failed"
-    fi
-
-    info "Installed Homebrew"
-
-    brew install git gnupg jq openssh zsh
-else
-    info "Homebrew is already installed"
-fi
--- platforms/macos.sh
+++ platforms/macos.sh
@@ -2,26 +2,6 @@
 
 head "Setting up macOS"
 
-ret=$(xcode-select --install)
-if [ $? -ne 0 ]; then
-    info "XCode command line tools are already installed"
-fi
-
-if [ -z "$NO_HOMEBREW" ] && [ ! -x "$(command -v brew)" ]; then
-    info "Install Homebrew"
-
-    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    if [ $? -ne 0 ]; then
-        fail "Homebrew installation failed"
-    fi
-
-    info "Installed Homebrew"
-
-    brew install git gnupg gpgme jq openssh pinentry-mac zsh
-else
-    info "Homebrew is already installed"
-fi
-
 if [ -z "$NO_HUSH_LOGIN" ]; then
     info "Touch $DOTTARGET/.hushlogin"
 
