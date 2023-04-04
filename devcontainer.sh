df7d4c9067daba120468f8691923c6b891bada4c
Yang Sheng Han <progamesigner@gmail.com>
2023-04-04 19:07:24 +0800

    Move git files to system-wide

--- devcontainer.sh
+++ devcontainer.sh
@@ -4,7 +4,7 @@ head "Setting up DevContainer"
 
 info "Configuring Git"
 
-cp "$PWD/git/attributes" ~/.gitattributes
-cp "$PWD/git/ignore" ~/.gitignore
+cp "$PWD/git/attributes" /etc/gitattributes
+cp "$PWD/git/ignore" /etc/gitignore
 
 info "Configured Git"
