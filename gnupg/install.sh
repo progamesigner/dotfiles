0d3731fb352dc69fe698cc5598fe633bf3c1980a
Yang Sheng Han <progamesigner@gmail.com>
2023-04-04 13:13:12 +0800

    Support GPGME-JSON for mailvelope browser extensions

--- gnupg/install.sh
+++ gnupg/install.sh
@@ -2,6 +2,32 @@
 
 head "Setting up GnuPG"
 
+install_browser_manifest () {
+    if [ -d "$(dirname "$1")" ]; then
+        manifest_path="$1/gpgmejson.json"
+
+        info "Installing browser manifest at \"$manifest_path\""
+
+        cat <<-EOF > "$manifest_path"
+{
+    "name": "gpgmejson",
+    "description": "JavaScript binding for GnuPG",
+    "path": "$(which gpgme-json)",
+    "type": "stdio",
+    "allowed_extensions": [
+        "jid1-AQqSMBYb0a8ADg@jetpack"
+    ],
+    "allowed_origins": [
+        "chrome-extension://dgcbddhdhjppfdfjpciagmmibadmoapc/",
+        "chrome-extension://kajibbejlbohfaggdiogboambcijhkke/"
+    ]
+}
+EOF
+
+        info "Installed browser manifest"
+    fi
+}
+
 if [ -z "$NO_GNUPG" ]; then
     info "Configuring GnuPG"
 
@@ -15,4 +41,31 @@ EOF
     fi
 
     info "Configured GnuPG"
+
+    if [ -n "$(which gpgme-json)" ]; then
+        info "Configuring GPGME-JSON"
+
+        install_browser_manifest "$HOME/.config/chromium/NativeMessagingHosts"
+        install_browser_manifest "$HOME/.config/google-chrome-beta/NativeMessagingHosts"
+        install_browser_manifest "$HOME/.config/google-chrome-unstable/NativeMessagingHosts"
+        install_browser_manifest "$HOME/.config/google-chrome/NativeMessagingHosts"
+        install_browser_manifest "$HOME/.config/microsoft-edge/NativeMessagingHosts"
+        install_browser_manifest "$HOME/.librewolf/native-messaging-hosts"
+        install_browser_manifest "$HOME/.mozilla/native-messaging-hosts"
+        install_browser_manifest "$HOME/.tor-browser/app/Browser/TorBrowser/Data/Browser/.mozilla/native-messaging-hosts"
+        install_browser_manifest "$HOME/.waterfox/native-messaging-hosts"
+        install_browser_manifest "$HOME/Library/Application Support/Chromium/NativeMessagingHosts"
+        install_browser_manifest "$HOME/Library/Application Support/Google/Chrome Beta/NativeMessagingHosts"
+        install_browser_manifest "$HOME/Library/Application Support/Google/Chrome Canary/NativeMessagingHosts"
+        install_browser_manifest "$HOME/Library/Application Support/Google/Chrome/NativeMessagingHosts"
+        install_browser_manifest "$HOME/Library/Application Support/Microsoft Edge Beta/NativeMessagingHosts"
+        install_browser_manifest "$HOME/Library/Application Support/Microsoft Edge Canary/NativeMessagingHosts"
+        install_browser_manifest "$HOME/Library/Application Support/Microsoft Edge Dev/NativeMessagingHosts"
+        install_browser_manifest "$HOME/Library/Application Support/Microsoft Edge/NativeMessagingHosts"
+        install_browser_manifest "$HOME/Library/Application Support/Mozilla/NativeMessagingHosts"
+        install_browser_manifest "$HOME/Library/Application Support/TorBrowser-Data/Browser/Mozilla/NativeMessagingHosts"
+        install_browser_manifest "$HOME/Library/Application Support/Vivaldi/NativeMessagingHosts"
+
+        info "Configured GPGME-JSON"
+    fi
 fi
