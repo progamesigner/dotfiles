25137d277dbcdad7f9d04d71db29a309075f6e95
Yang Sheng Han <progamesigner@gmail.com>
2023-04-04 19:04:21 +0800

    Using copying instead of configuring

--- devcontainer.sh
+++ devcontainer.sh
@@ -4,7 +4,7 @@ head "Setting up DevContainer"
 
 info "Configuring Git"
 
-git config --system core.attributesFile "$PWD/git/attributes"
-git config --system core.excludesFile "$PWD/git/ignore"
+cp "$PWD/git/attributes" ~/.gitattributes
+cp "$PWD/git/ignore" ~/.gitignore
 
 info "Configured Git"
