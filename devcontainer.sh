bd39cf6d1399f793c486a0bbaa7053e502fedaaa
Yang Sheng Han <progamesigner@gmail.com>
2023-04-04 19:00:30 +0800

    Fix git config in DevContainers

--- devcontainer.sh
+++ devcontainer.sh
@@ -4,7 +4,7 @@ head "Setting up DevContainer"
 
 info "Configuring Git"
 
-git config --global core.attributesFile "$PWD/git/attributes"
-git config --global core.excludesFile "$PWD/git/ignore"
+git config --system core.attributesFile "$PWD/git/attributes"
+git config --system core.excludesFile "$PWD/git/ignore"
 
 info "Configured Git"
