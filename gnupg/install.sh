4a6712728c777e5c8b2f1c5e4893dd1dcb8d3ae7
Yang Sheng Han <progamesigner@gmail.com>
2025-03-03 16:33:12 +0800

    Fix GnuPG integration for Gecko-based browsers

--- gnupg/install.sh
+++ gnupg/install.sh
@@ -3,12 +3,26 @@
 head "Setting up GnuPG"
 
 install_browser_manifest () {
-    if [ -d "$(dirname "$1")" ]; then
-        manifest_path="$1/gpgmejson.json"
+    if [ -d "$(dirname "$2")" ]; then
+        manifest_path="$2/gpgmejson.json"
 
         info "Installing browser manifest at \"$manifest_path\""
 
-        cat <<-EOF > "$manifest_path"
+        if [ "$1" = "chromium" ]; then
+            cat <<-EOF > "$manifest_path"
+{
+    "name": "gpgmejson",
+    "description": "JavaScript binding for GnuPG",
+    "path": "$(which gpgme-json)",
+    "type": "stdio",
+    "allowed_origins": [
+        "chrome-extension://dgcbddhdhjppfdfjpciagmmibadmoapc/",
+        "chrome-extension://kajibbejlbohfaggdiogboambcijhkke/"
+    ]
+}
+EOF
+        elif [ "$1" = "gecko" ]; then
+            cat <<-EOF > "$manifest_path"
 {
     "name": "gpgmejson",
     "description": "JavaScript binding for GnuPG",
@@ -16,13 +30,10 @@ install_browser_manifest () {
     "type": "stdio",
     "allowed_extensions": [
         "jid1-AQqSMBYb0a8ADg@jetpack"
-    ],
-    "allowed_origins": [
-        "chrome-extension://dgcbddhdhjppfdfjpciagmmibadmoapc/",
-        "chrome-extension://kajibbejlbohfaggdiogboambcijhkke/"
     ]
 }
 EOF
+        fi
 
         info "Installed browser manifest"
     fi
@@ -45,26 +56,33 @@ EOF
     if [ -n "$(which gpgme-json)" ]; then
         info "Configuring GPGME-JSON"
 
-        install_browser_manifest "$HOME/.config/chromium/NativeMessagingHosts"
-        install_browser_manifest "$HOME/.config/google-chrome-beta/NativeMessagingHosts"
-        install_browser_manifest "$HOME/.config/google-chrome-unstable/NativeMessagingHosts"
-        install_browser_manifest "$HOME/.config/google-chrome/NativeMessagingHosts"
-        install_browser_manifest "$HOME/.config/microsoft-edge/NativeMessagingHosts"
-        install_browser_manifest "$HOME/.librewolf/native-messaging-hosts"
-        install_browser_manifest "$HOME/.mozilla/native-messaging-hosts"
-        install_browser_manifest "$HOME/.tor-browser/app/Browser/TorBrowser/Data/Browser/.mozilla/native-messaging-hosts"
-        install_browser_manifest "$HOME/.waterfox/native-messaging-hosts"
-        install_browser_manifest "$HOME/Library/Application Support/Chromium/NativeMessagingHosts"
-        install_browser_manifest "$HOME/Library/Application Support/Google/Chrome Beta/NativeMessagingHosts"
-        install_browser_manifest "$HOME/Library/Application Support/Google/Chrome Canary/NativeMessagingHosts"
-        install_browser_manifest "$HOME/Library/Application Support/Google/Chrome/NativeMessagingHosts"
-        install_browser_manifest "$HOME/Library/Application Support/Microsoft Edge Beta/NativeMessagingHosts"
-        install_browser_manifest "$HOME/Library/Application Support/Microsoft Edge Canary/NativeMessagingHosts"
-        install_browser_manifest "$HOME/Library/Application Support/Microsoft Edge Dev/NativeMessagingHosts"
-        install_browser_manifest "$HOME/Library/Application Support/Microsoft Edge/NativeMessagingHosts"
-        install_browser_manifest "$HOME/Library/Application Support/Mozilla/NativeMessagingHosts"
-        install_browser_manifest "$HOME/Library/Application Support/TorBrowser-Data/Browser/Mozilla/NativeMessagingHosts"
-        install_browser_manifest "$HOME/Library/Application Support/Vivaldi/NativeMessagingHosts"
+        # for chromium-based browsers on Linux
+        install_browser_manifest chromium "$HOME/.config/chromium/NativeMessagingHosts"
+        install_browser_manifest chromium "$HOME/.config/google-chrome-beta/NativeMessagingHosts"
+        install_browser_manifest chromium "$HOME/.config/google-chrome-unstable/NativeMessagingHosts"
+        install_browser_manifest chromium "$HOME/.config/google-chrome/NativeMessagingHosts"
+        install_browser_manifest chromium "$HOME/.config/microsoft-edge/NativeMessagingHosts"
+
+        # for gecko-based browsers on Linux
+        install_browser_manifest gecko "$HOME/.librewolf/native-messaging-hosts"
+        install_browser_manifest gecko "$HOME/.mozilla/native-messaging-hosts"
+        install_browser_manifest gecko "$HOME/.tor-browser/app/Browser/TorBrowser/Data/Browser/.mozilla/native-messaging-hosts"
+        install_browser_manifest gecko "$HOME/.waterfox/native-messaging-hosts"
+
+        # for chromium-based browsers on macOS
+        install_browser_manifest chromium "$HOME/Library/Application Support/Chromium/NativeMessagingHosts"
+        install_browser_manifest chromium "$HOME/Library/Application Support/Google/Chrome Beta/NativeMessagingHosts"
+        install_browser_manifest chromium "$HOME/Library/Application Support/Google/Chrome Canary/NativeMessagingHosts"
+        install_browser_manifest chromium "$HOME/Library/Application Support/Google/Chrome/NativeMessagingHosts"
+        install_browser_manifest chromium "$HOME/Library/Application Support/Microsoft Edge Beta/NativeMessagingHosts"
+        install_browser_manifest chromium "$HOME/Library/Application Support/Microsoft Edge Canary/NativeMessagingHosts"
+        install_browser_manifest chromium "$HOME/Library/Application Support/Microsoft Edge Dev/NativeMessagingHosts"
+        install_browser_manifest chromium "$HOME/Library/Application Support/Microsoft Edge/NativeMessagingHosts"
+        install_browser_manifest chromium "$HOME/Library/Application Support/Vivaldi/NativeMessagingHosts"
+
+        # for gecko-based browsers on macOS
+        install_browser_manifest gecko "$HOME/Library/Application Support/Mozilla/NativeMessagingHosts"
+        install_browser_manifest gecko "$HOME/Library/Application Support/TorBrowser-Data/Browser/Mozilla/NativeMessagingHosts"
 
         info "Configured GPGME-JSON"
     fi
